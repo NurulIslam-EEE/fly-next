c5b7f6a3e6f1577295ae5c7749a41bcf487c0c9a
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-10-29 00:20:15 +0600

    loader added

---
+++ src/components/LoaderPlane.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoaderPlane() {
+  return (
+    <div>
+      <div className="earth w-[160px] h-[160px] bg-[url('/earth.gif')] rounded-[100%]"></div>
+    </div>
+  );
+}
+
+export default LoaderPlane;
